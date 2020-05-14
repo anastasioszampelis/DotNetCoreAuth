009b9069659876ad67a1b27cf61fcd3f3eaad573
Anastasios Zampelis <anastasioszampelis@gmail.com>
2020-05-15 02:22:28 +0300

    Initial

---
+++ Basics/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.Username = User.Identity.Name;
+            }
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if(password == "test")
+            {
+                var grandmaClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim("Grandma.Says", "Very nice boi."),
+                };
+
+                var licenseClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, "Bob K Foo"),
+                    new Claim("DrivingLicense", "A+"),
+                };
+
+                var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+                var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
+
+                var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+
+                HttpContext.SignInAsync(userPrincipal);
+            }
+            
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Basics/Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace Basics
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth", config =>
+                {
+                    config.Cookie.Name = "Grandmas.Cookie";
+                    config.LoginPath = "/Home/Login";
+                    config.ExpireTimeSpan = System.TimeSpan.FromMinutes(1);
+                });
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // who are you?
+            app.UseAuthentication();
+
+            // are you allowed?
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
